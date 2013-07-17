b3efdcb95915823953128baa3b2b502961813634
sbondada <sbondada@buffalo.edu>
2013-07-17 18:05:08 -0400

    SVO extracter with thw count completed

--- src/Parser/SVOextracter.java
+++ src/Parser/SVOextracter.java
@@ -1,7 +1,7 @@
 package Parser;
 
-
-	import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
 import edu.stanford.nlp.ling.HasWord;
@@ -12,60 +12,111 @@ import edu.stanford.nlp.trees.GrammaticalStructureFactory;
 import edu.stanford.nlp.trees.PennTreebankLanguagePack;
 import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.trees.TreebankLanguagePack;
+import edu.stanford.nlp.trees.TypedDependency;
 
-public class SVOextracter {
-	  /**
-	   * The main method demonstrates the easiest way to load a parser.
-	   * Simply call loadModel and specify the path, which can either be a
-	   * file or any resource in the classpath.  For example, this
-	   * demonstrates loading from the models jar file, which you need to
-	   * include in the classpath for ParserDemo to work.
-	   */
-	  public static void main(String[] args) {
-	    LexicalizedParser lp = LexicalizedParser.loadModel("edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz");
-	    if (args.length > 0) {
-	      demoDP(lp, args[0]);
-//	    } else {
-//	      demoAPI(lp);
-	    }
+public class SVOextracter 
+{
+	  HashMap<String,Integer> subjverb;
+	  HashMap<String,Integer> verbobj;
+	  HashMap<String,Integer> subjverbobj;
+	  
+	  public SVOextracter()
+	  {
+		  subjverb=new HashMap<>();
+		  verbobj=new HashMap<>();
+		  subjverbobj=new HashMap<>();
+	  }
+	  public static void main(String[] args) 
+	  {
+		  LexicalizedParser lp = LexicalizedParser.loadModel("edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz");
+		  SVOextracter s=new SVOextracter();
+		  s.sentenceSplit(lp, args[0]);
 	  }
 
-	  /**
-	   * demoDP demonstrates turning a file into tokens and then parse
-	   * trees.  Note that the trees are printed by calling pennPrint on
-	   * the Tree object.  It is also possible to pass a PrintWriter to
-	   * pennPrint if you want to capture the output.
-	   */
-	  public static void demoDP(LexicalizedParser lp, String filename) {
-	    // This option shows loading and sentence-segmenting and tokenizing
-	    // a file using DocumentPreprocessor.
-	    TreebankLanguagePack tlp = new PennTreebankLanguagePack();
-	    GrammaticalStructureFactory gsf = tlp.grammaticalStructureFactory();
-	    // You could also create a tokenizer here (as below) and pass it
-	    // to DocumentPreprocessor
-	    for (List<HasWord> sentence : new DocumentPreprocessor(filename)) {
-	      Tree parse = lp.apply(sentence);
-	     // parse.pennPrint();
-	      System.out.println();
-
-	      GrammaticalStructure gs = gsf.newGrammaticalStructure(parse);
-	      // the main purpose to get the dependencies in Collection
-	      Collection tdl = gs.typedDependenciesCCprocessed();
-	      System.out.println("Total dependencies  "+gs.dependencies());
-	      System.out.println("Relation  "+gs.dependencies().iterator().next());
-	      System.out.println("Governor  "+gs.dependencies().iterator().next().governor());
-	      System.out.println("Dependent  "+gs.dependencies().iterator().next().dependent());
-	      System.out.println("name  "+gs.dependencies().iterator().next().name());
-	      
-	      System.out.println("--------------------------------------------------------------------------------------------------------");
-	      //this seems to work
-	      System.out.println("Total Dependencies  "+gs.typedDependencies());
-	      System.out.println("Relation  "+gs.typedDependencies().iterator().next());
-	      System.out.println("Governor  "+gs.typedDependencies().iterator().next().gov());
-	      System.out.println("Dependent  "+gs.typedDependencies().iterator().next().dep());
-	      System.out.println("Dependency type  "+gs.typedDependencies().iterator().next().reln());
-	      
-	    }
+	  public void sentenceSplit(LexicalizedParser lp, String filename) 
+	  {
+		  TreebankLanguagePack tlp = new PennTreebankLanguagePack();
+		  GrammaticalStructureFactory gsf = tlp.grammaticalStructureFactory();
+		  for (List<HasWord> sentence : new DocumentPreprocessor(filename)) 
+		  {
+			  Tree parse = lp.apply(sentence);
+			  GrammaticalStructure gs = gsf.newGrammaticalStructure(parse); 
+			  extractSVO(gs);
+		  }
+	  	}
+	  public void extractSVO(GrammaticalStructure gs)
+	  {
+		  HashMap<String,String> SV =new HashMap<>();
+		  HashMap<String,String> VO =new HashMap<>();
+		  
+//		  System.out.println("Total Dependencies  "+gs.typedDependencies());
+//		  System.out.println("Relation  "+gs.typedDependencies().iterator().next());
+//		  System.out.println("Governor  "+gs.typedDependencies().iterator().next().gov());
+//		  System.out.println("Dependent  "+gs.typedDependencies().iterator().next().dep());
+//		  System.out.println("Dependency type  "+gs.typedDependencies().iterator().next().reln());
+		  Iterator<TypedDependency> Depitr = gs.typedDependencies().iterator();
+		  while (Depitr.hasNext())
+		  {
+			 TypedDependency Dep = Depitr.next();
+			 if(Dep.reln().getShortName()=="nsubj")
+			 {
+				 String[] subject=Dep.dep().toString().split("-");
+				 String[] verb=Dep.gov().toString().split("-");
+				 String key=subject[0]+"-"+verb[0];
+				 SV.put(Dep.dep().toString(),Dep.gov().toString());
+				 if(subjverb.containsKey(key))
+				 {
+					 subjverb.put(key,subjverb.get(key)+1);
+				 }
+				 else
+				 {
+					 subjverb.put(key,1);
+				 }		
+				 System.out.println(key);
+			 }
+			 if(Dep.reln().getShortName()=="dobj")
+			 {
+			 	String[] object=Dep.dep().toString().split("-");
+			 	String[] verb=Dep.gov().toString().split("-");
+			 	String key=verb[0]+"-"+object[0];
+			 	VO.put(Dep.gov().toString(),Dep.dep().toString());
+				 if(verbobj.containsKey(key))
+				 {
+					 verbobj.put(key,verbobj.get(key)+1);
+				 }
+				 else
+				 {
+					 verbobj.put(key,1);
+				 }
+				 System.out.println(key);
+			 }
+			 
+		  }
+		  Iterator SVOitr=SV.keySet().iterator();
+		  while(SVOitr.hasNext())
+		  {
+			  String s=SVOitr.next().toString();
+			  String v=SV.get(s);
+			 // System.out.println(VO.keySet());
+			  if (VO.containsKey(v))
+			  {
+				  String[] sparts=s.split("-");
+				  String[] vparts=v.split("-");
+				  subjverb.put(sparts[0]+"-"+vparts[0],subjverb.get(sparts[0]+"-"+vparts[0])-1);
+				  String[] oparts=VO.get(v).split("-");
+				  verbobj.put(vparts[0]+"-"+oparts[0],verbobj.get(vparts[0]+"-"+oparts[0])-1);
+				  String SVOkey=sparts[0]+"-"+vparts[0]+"-"+oparts[0];
+				  if(subjverbobj.containsKey(SVOkey))
+				  {
+					  subjverbobj.put(SVOkey,subjverbobj.get(SVOkey)+1);
+				  }
+				  else
+				  {
+					  subjverbobj.put(SVOkey,1);
+				  }
+			  }
+		  }
+		//  System.out.println(subjverbobj.values());
 	  }
 
 }
